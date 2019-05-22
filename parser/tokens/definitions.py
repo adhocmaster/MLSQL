8f987eef0b66518e4c23961c0d177b1c72dc745d
Adhocmaster <adhocmaster@live.com>
2019-05-22 14:25:08 -0700

    more

--- parser/tokens/definitions.py
+++ parser/tokens/definitions.py
@@ -1,7 +1,6 @@
 modelTokens = [
     'CREATE',
     'MODEL',
-    'ESTIMATOR',
     'REGULARIZER',
     'TYPE',
     'FORMULA',
@@ -22,12 +21,17 @@ def t_CREATE(t):
     r'CREATE'
     return t
 def t_MODEL(t):
-    r'MODEL'
+    r'MODEL|ESTIMATOR'
     return t
     
 def t_REGULARIZER(t):
     r'REGULARIZER'
     return t
+    
 def t_TYPE(t):
+    r'TYPE'
+    return t
+    
+def t_FORMULA(t):
     r'TYPE'
     return t
